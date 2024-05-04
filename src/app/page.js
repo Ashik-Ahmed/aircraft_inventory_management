1f21b4183c24e5f5a87cd26c586aec1ad7af276e
ashik-ahmed <ashikahmed121@gmail.com>
2024-05-04 22:56:29 +0600

    aircraft add with photo added

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,17 @@ import AircraftCard from "./components/AircraftCard/AircraftCard";
 import { Button } from "primereact/button";
 import { useState } from "react";
 import { InputText } from "primereact/inputtext";
+import { useForm } from "react-hook-form";
+import { FileUpload } from "primereact/fileupload";
 
 
 export default function Home() {
 
   const [addNew, setAddNew] = useState(false);
+  const [image, setImage] = useState(null);
+
+  const { register, control, formState: { errors }, handleSubmit, reset } = useForm();
+
 
   const aircrafts = [
     { id: 1, title: 'Aircraft 1', imageSrc: 'https://cdn.jetphotos.com/full/6/73985_1579885991.jpg', imageAlt: 'Some image' },
@@ -24,9 +30,16 @@ export default function Home() {
     { id: 10, title: 'Aircraft 10', imageSrc: 'https://cdn.jetphotos.com/full/6/73985_1579885991.jpg', imageAlt: 'Some image' },
   ]
 
-  const handleAddNewAircraft = () => {
-    console.log("Add New Aircraft");
+  const handleAddNewAircraft = (data) => {
+    console.log("Add New Aircraft", data);
+    console.log(image);
+    setAddNew(false);
+    reset();
   }
+
+  const handlePhotoChange = (event) => {
+    setImage(event.target.files[0]);
+  };
 
   return (
     <main>
@@ -44,12 +57,39 @@ export default function Home() {
 
       <Dialog header="Add New Aircraft" visible={addNew} onHide={() => setAddNew(false)}
         style={{ width: '35vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
-        <form className="flex flex-col gap-2 mt-4">
+        <form onSubmit={handleSubmit(handleAddNewAircraft)} className="flex flex-col gap-2 mt-4">
 
-          <InputText placeholder="Aircraft Name" className="border-2" />
-          <InputText placeholder="Aircraft ID" className="border-2" />
+          {/* <InputText placeholder="Aircraft Name" className="border-2" />
+          <InputText placeholder="Aircraft ID" className="border-2" /> */}
+          <div className='w-full'>
+            <InputText
+              {...register("aircraftName", { required: "Aircraft Name is required" })}
+              placeholder="Aircraft Name*" className='w-full border p-1' />
+            {errors.aircraftName?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.aircraftName.message}</span>}
+          </div>
+          <div className='w-full'>
+            <InputText
+              {...register("aircraftId", { required: "Aircraft ID is required" })}
+              placeholder="Aircraft ID*" className='w-full border p-1' />
+            {errors.aircraftId?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.aircraftId.message}</span>}
+          </div>
+          {/* 
+          <div className='w-full'>
+            <FileUpload
+              {...register("aircraftPhoto", { required: "Photo is required" })}
+              mode="basic" accept="image/*" maxFileSize={1000000} chooseLabel="Browse Photo" />
+            {errors.aircraftPhoto?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.aircraftPhoto.message}</span>}
+          </div> */}
+          <div className='mt-2'>
+            <input
+              {...register("aircraftPhoto", { required: "Photo is required" })}
+              onChange={handlePhotoChange} name='file' type="file" className='w-full border border-violet-600' />
+            {errors.aircraftPhoto?.type === 'required' && <span className='text-xs text-red-500' role="alert">{errors.aircraftPhoto.message}</span>}
+          </div>
 
-          <Button type="submit" label="Submit" className="bg-blue-400 text-white w-fit p-1"></Button>
+          <div>
+            <Button type="submit" label="Submit" className="bg-blue-400 text-white w-fit p-1"></Button>
+          </div>
         </form>
       </Dialog>
     </main>
